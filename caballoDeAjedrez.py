713958de91b3ab6f42ab607aa143c7764fbc8f9c
JhoanSB0608 <156533638+JhoanSB0608@users.noreply.github.com>
2024-02-01 21:23:57 -1000

    feat: :sparkles: Este programa verifica los movimientos posibles de un caballo en un tablero de ajedrez y muestra las coordenadas a las que puede saltar el caballo a partir de una posición dada.
    Este programa define dos funciones: coordenada_valida verifica si las coordenadas dadas (fila, columna) están dentro del rango permitido (1-8), y movimientos_posibles_caballo calcula los posibles movimientos de un caballo en un tablero de ajedrez desde la posición dada (fila, columna). Luego, el programa solicita al usuario que ingrese las coordenadas del caballo, verifica si son válidas y, en caso afirmativo, imprime los posibles movimientos del caballo desde esa posición.

---
+++ caballoDeAjedrez.py
@@ -0,0 +1,29 @@
+def coordenada_valida (fila, columna):
+    return 1 <= fila <= 8 and 1 <= columna <= 8
+
+def movimientos_posibles_caballo (fila, columna):
+    movimientos = []
+    posibles_movimientos = [(-2, -1), (-2, 1), (-1, -2), (1, -2), (1, 2), (2, -1), (2, 1)]
+
+    for mov in posibles_movimientos:
+        nueva_fila = fila + mov [0]
+        nueva_columna = columna + mov [1]
+
+        if coordenada_valida(nueva_fila, nueva_columna):
+            movimientos.append((nueva_fila, nueva_columna))
+    
+    return movimientos
+
+try:
+    print("Ingrese coordenadas del caballo.")
+    fila = int(input("Fila (1-8): "))
+    columna= int(input("Columna (1-8): "))
+
+    if coordenada_valida(fila, columna):
+        resultados = movimientos_posibles_caballo (fila, columna)
+        print(f"El caballo puede saltar de {fila} {columna} a: {resultados}")
+    else:
+        print ("Posicion invalida.")
+
+except ValueError:
+    print("Error: Ingrese valores numericos para las coordenadas.")
